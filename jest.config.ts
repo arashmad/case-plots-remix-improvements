0e1bf751a2ec8550fde9c421fef62d456640ce88
Arash <arashmad.dev@gmail.com>
2024-12-07 17:19:01 +0100

    set coverage settings

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,9 @@ import type { Config } from "jest";
 
 const config: Config = {
   testEnvironment: "jest-environment-jsdom",
+  collectCoverage: true,
+  coverageDirectory: "coverage", // Output directory for coverage reports
+  coverageReporters: ["text", "lcov"], // Include lcov for coverage tools like Codecov
   moduleNameMapper: {
     "^~/(.*)$": "<rootDir>/app/$1", // Ensure paths resolve correctly
   },
