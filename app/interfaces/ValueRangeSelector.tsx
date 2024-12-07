a72de553c3edaf6fd49fee3a1d3106b2ab222356
Arash <arashmad.dev@gmail.com>
2024-12-07 18:41:19 +0100

    a directory for component interfaces

---
+++ app/interfaces/ValueRangeSelector.tsx
@@ -0,0 +1,27 @@
+/**
+ * Interface for a value range selector component.
+ *
+ * @interface IValueRangeSelector
+ */
+export interface IValueRangeSelector {
+  /**
+   * The minimum value of the range.
+   *
+   * @type {number | string | undefined}
+   */
+  minValue: number | string | undefined;
+
+  /**
+   * The maximum value of the range.
+   *
+   * @type {number | string | undefined}
+   */
+  maxValue: number | string | undefined;
+
+  /**
+   * Callback function called when the range changes.
+   *
+   * @param {React.ChangeEvent<HTMLInputElement>} e - The change event
+   */
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
