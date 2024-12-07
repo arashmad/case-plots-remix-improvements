bbc9939698f10f3f11a3df8bfc7c2d2ecc0dae69
Arash <arashmad.dev@gmail.com>
2024-12-07 14:17:34 +0100

    added missing jdocs

--- app/components/Form/ValueRangeSelector/ValueRangeSelector.tsx
+++ app/components/Form/ValueRangeSelector/ValueRangeSelector.tsx
@@ -1,11 +1,57 @@
 import { useState } from "react";
 
+/**
+ * Interface for a value range selector component.
+ *
+ * @interface IValueRangeSelector
+ */
 interface IValueRangeSelector {
+  /**
+   * The minimum value of the range.
+   *
+   * @type {number | string | undefined}
+   */
   minValue: number | string | undefined;
+
+  /**
+   * The maximum value of the range.
+   *
+   * @type {number | string | undefined}
+   */
   maxValue: number | string | undefined;
+
+  /**
+   * Callback function called when the range changes.
+   *
+   * @param {React.ChangeEvent<HTMLInputElement>} e - The change event
+   */
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
+/**
+ * A component that allows the user to select a range of values.
+ * The component contains two input fields: one for the minimum value
+ * and one for the maximum value.
+ *
+ * The component uses the `useState` hook to store the current range
+ * selected by the user.
+ *
+ * The component uses the `onChange` callback function to notify the
+ * parent component whenever the range is changed.
+ *
+ * @example
+ * <ValueRangeSelector
+ *   minValue={0}
+ *   maxValue={100}
+ *   onChange={(e) => console.log(e.target.value)}
+ * />
+ *
+ * @param {IValueRangeSelector} props
+ * @prop {number | string | undefined} minValue The minimum value of the range.
+ * @prop {number | string | undefined} maxValue The maximum value of the range.
+ * @prop {(e: React.ChangeEvent<HTMLInputElement>) => void} onChange The callback function called when the range changes.
+ * @returns {ReactElement} A value range selector component
+ */
 const ValueRangeSelector: React.FC<IValueRangeSelector> = (props) => {
   const { minValue, maxValue, onChange } = props;
 
